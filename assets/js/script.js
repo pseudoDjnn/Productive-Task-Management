9ea417dca0d8504d852f2df22e3c96ea74e0743f
p5eudoDjnn <alexander.havers@gmail.com>
2022-07-30 19:26:20 -0400

    fixed html error for code to run

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,38 @@
-let buttonEl = document.querySelector("#save-task");
-let tasksToDoEl = document.querySelector("#tasks-to-do");
+// var buttonEl = document.querySelector("#save-task");
+// let formEl = document.querySelector("#task-form");
+// let tasksToDoEl = document.querySelector("#tasks-to-do");
 
-// buttonEl.addEventListener("click", function() {
-//   var listItemEl = document.createElement("li");
+// let createTaskHandler = function (event) {
+//   event.preventDefault();
+
+//   let listItemEl = document.createElement("li");
 //   listItemEl.className = "task-item";
-//   listItemEl.textContent = "This is a new task.";
+//   listItemEl.textContent = "This is a new task";
 //   tasksToDoEl.appendChild(listItemEl);
-// });
+// };
 
-let createTaskHandler = function () {
-  let listItemEl = document.createElement("li");
+// // buttonEl.addEventListener("click", createTaskHandler);
+// formEl.addEventListener("submit", createTaskHandler);
+
+// console.log(event);
+
+var formEl = document.querySelector("#task-form");
+console.log("🚀 ~ file: script.js ~ line 20 ~ formEl", formEl);
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+console.log("🚀 ~ file: script.js ~ line 22 ~ tasksToDoEl", tasksToDoEl);
+
+var createTaskHandler = function (event) {
+  event.preventDefault();
+  // event.preventDefault();
+
+  // var taskNameInput = document.querySelector("input[name='task-name']");
+  // console.log(taskNameInput);
+
+  var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "this is a new task";
+  listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
+// formEl.addEventListener("submit", createTaskHandler);
